92117968487d59eea1844abb37664a1fc4b700e9
Evan Yan <1370204201@qq.com>
2016-06-06 17:04:39 +0800

    清楚测试代码

--- route/route.js
+++ route/route.js
@@ -3,11 +3,7 @@
  */
 
 var xysearch = require('../route/xysearch');
-var person_save_test = require('../route/person_save_test');
-var person_list = require('../route/person_list');
 
 module.exports = function (app) {
     app.get('/xysearch/:name', xysearch);
-    app.get('/mongodbtest',person_save_test);
-    app.get('/mongodblist',person_list);
 };
